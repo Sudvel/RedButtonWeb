213112abe4d0e150e8f2dfc19d3211425fb911aa
Pavel <sudvel@yandex.ru>
2026-09-10 17:04:33 +0300

    beta db created

--- control_interface/homepage/models.py
+++ control_interface/homepage/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class NetworkDevices(models.Model):
+    host_name = models.CharField(max_length=128, null=True, blank=True)
+    ip_address = models.GenericIPAddressField(protocol='both')
+    device_type = models.CharField(max_length=128)
+    profile_name = models.CharField(max_length=128, null=True, blank=True)
+    user_login = models.CharField(max_length=128)
+    
+
+
+class Ports(models.Model):
+    port_name = models.CharField(max_length=128)
+    device_ip = models.ForeignKey(
+        NetworkDevices, 
+        on_delete=models.CASCADE
+    )
+
